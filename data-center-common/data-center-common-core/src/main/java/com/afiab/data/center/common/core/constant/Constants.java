43e5e683b4a49236061a73e85f248a0c6434ee88
ZHANGBAIFA <1350862999@qq.com>
2023-08-24 15:40:34 +0800

    异常处理

---
+++ data-center-common/data-center-common-core/src/main/java/com/afiab/data/center/common/core/constant/Constants.java
@@ -0,0 +1,92 @@
+package com.afiab.data.center.common.core.constant;
+
+/**
+ * @Version 1.0
+ * @Author ZHANGBAIFA
+ * @Date 2023/8/24 15:26
+ * @Description:
+ */
+public class Constants {
+    /**
+     * UTF-8 字符集
+     */
+    public static final String UTF8 = "UTF-8";
+
+    public static final String AT_SIGN = "@";
+
+    public static final String SHARP = "#";
+
+    /**
+     * 制表符
+     */
+    public static final String LINE_TAB = "\t";
+
+    /**
+     * 换行符
+     */
+    public static final String LINE_SEPARATOR = "\r\n";
+
+    /**
+     * 登录成功
+     */
+    public static final String LOGIN_SUCCESS = "Success";
+
+    /**
+     * 注销
+     */
+    public static final String LOGOUT = "Logout";
+
+    /**
+     * 登录失败
+     */
+    public static final String LOGIN_FAIL = "Error";
+
+    /**
+     * 自动去除表前缀
+     */
+    public static final String AUTO_REOMVE_PRE = "true";
+
+    /**
+     * 当前记录起始索引
+     */
+    public static final String PAGE_NUM = "pageNum";
+
+    /**
+     * 每页显示记录数
+     */
+    public static final String PAGE_SIZE = "pageSize";
+
+    /**
+     * 排序列
+     */
+    public static final String ORDER_BY_COLUMN = "sortField";
+
+    /**
+     * 排序的方向 "desc" 或者 "asc".
+     */
+    public static final String IS_ASC = "sortOrder";
+
+    /**
+     * 是否为管理员
+     */
+    public static final String IS_ADMIN = "is_admin";
+
+    public static final String USER_NAME = "userName";
+
+    public static final String USER_CODE = "userCode";
+
+    /**
+     * 当前组织ID
+     */
+    public static final String CURRENT_ORG_CODE = "orgCode";
+
+    public static final String RESOURCE_PREFIX = "/profile";
+
+    /**
+     * 数据资源下载权限默认id码（必须小写以区分）提供给用户管理配置使用,
+     */
+    public static final String DATA_DOWNLOAD_SRC_ID = "download";
+
+    public static final String DOWNLOAD_FILE = "download_limit";
+
+}
--- data-center-common/data-center-common-core/src/main/java/com/afiab/data/center/common/core/exception/BusinessException.java
+++ data-center-common/data-center-common-core/src/main/java/com/afiab/data/center/common/core/exception/BusinessException.java
@@ -1,10 +1,83 @@
 package com.afiab.data.center.common.core.exception;
 
+import com.afiab.data.center.common.core.base.domain.BusinessCodeEnum;
+
 /**
  * @Version 1.0
  * @Author ZHANGBAIFA
  * @Date 2023/8/24 11:03
  * @Description:
  */
-public class BusinessException {
+public class BusinessException extends RuntimeException {
+    private static final long serialVersionUID = -3593919652547622028L;
+    private String code;
+    private String message;
+    private BusinessCodeEnum businessCodeEnum;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public BusinessCodeEnum getBusinessCodeEnum() {
+        return businessCodeEnum;
+    }
+
+    public void setBusinessCodeEnum(BusinessCodeEnum businessCodeEnum) {
+        this.businessCodeEnum = businessCodeEnum;
+    }
+
+
+    public BusinessException() {
+        super();
+    }
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String code,String message) {
+        super(message);
+        this.code = code;
+        this.message = message;
+    }
+
+    public BusinessException(BusinessCodeEnum code) {
+        super(code.getMsg());
+        this.businessCodeEnum = code;
+    }
+
+    public BusinessException(BusinessCodeEnum code, String tipData) {
+        super(code.getMsg());
+        this.businessCodeEnum = code;
+        this.message = tipData;
+    }
+
+    public BusinessException(BusinessCodeEnum code, Throwable cause) {
+        super(code.getMsg(),cause);
+        this.businessCodeEnum = code;
+    }
+
+    public BusinessException(Throwable cause) {
+        super(cause);
+    }
+
+    protected BusinessException(String message, Throwable cause,
+                                boolean enableSuppression,
+                                boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
 }
