edd539065e6c022bc51b1337353e3944aeeb874f
ZHANGBAIFA <1350862999@qq.com>
2023-09-07 15:10:14 +0800

    对外元数据接口

---
+++ data-center-metadata/data-center-metadata-api/src/main/java/com/afiab/data/center/metadata/api/facade/MetadataDatasourceFacade.java
@@ -0,0 +1,13 @@
+package com.afiab.data.center.metadata.api.facade;
+
+/**
+ * @Version 1.0
+ * @Author ZHANGBAIFA
+ * @Date 2023/9/7 15:09
+ * @Description: 对外提供元数据接口
+ */
+
+public interface MetadataDatasourceFacade {
+
+
+}
