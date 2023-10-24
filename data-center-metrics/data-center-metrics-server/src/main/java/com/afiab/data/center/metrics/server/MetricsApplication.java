e4ac6b89eb697a7c490906d8f978185721dfa3b8
ZHANGBAIFA <1350862999@qq.com>
2023-10-24 17:39:20 +0800

    指标项目初始化

--- data-center-metrics/data-center-metrics-server/src/main/java/com/afiab/data/center/metrics/server/MetricsApplication.java
+++ data-center-metrics/data-center-metrics-server/src/main/java/com/afiab/data/center/metrics/server/MetricsApplication.java
@@ -1,13 +1,21 @@
 package com.afiab.data.center.metrics.server;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @Version 1.0
  * @Author ZHANGBAIFA
  * @Date 2023/8/24 18:50
  * @Description:
  */
+@SpringBootApplication(scanBasePackages={"com.afiab.data.center.metrics"})
 public class MetricsApplication {
+    private static final Logger log = LoggerFactory.getLogger(MetricsApplication.class);
     public static void main(String[] args) {
-
+        SpringApplication.run(MetricsApplication.class, args);
+        log.info("MetricsApplication started successfully");
     }
 }
